435685a5107b289c3e189378ed3e3778acef207e
Thomas Cannon <tcannon@codemonkeydev.com>
2013-06-22 00:57:03 -0400

    Wrote Tests for Todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,11 @@
 class Todo < ActiveRecord::Base
-  attr_accessible :boolean, :list_id, :name
+  attr_accessible :completed, :list_id, :name
+  validates :name, :presence => true
+
   belongs_to :list
+
+
+
+  scope :uncompleted, where(:completed => false)
+  scope :completed, 	where(:completed => true)
 end
--- test/unit/todo_test.rb
+++ test/unit/todo_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "A Name should be required" do
+  	todo = Todo.new(:name => nil)
+  	assert_equal todo.valid?, false
+  	assert_not_nil todo.errors[:name]
+  end
+
+  test "We can search for all uncompleted todos" do
+  	uncompleted_todos = Todo.uncompleted
+  	assert_equal uncompleted_todos.include?(todos(:uncompleted)), true
+  	assert_equal uncompleted_todos.include?(todos(:completed)), false
+  end
+
+  test "We can search for all completed todos" do
+  	completed_todos = Todo.completed
+  	assert_equal completed_todos.include?(todos(:completed)), true
+  	assert_equal completed_todos.include?(todos(:uncompleted)), false
+  end
 end
