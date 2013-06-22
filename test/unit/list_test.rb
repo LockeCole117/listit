e96d98a855e4631249f437085db4caaa99b825d9
Thomas Cannon <tcannon@codemonkeydev.com>
2013-06-22 00:56:40 -0400

    Wrote Tests for List Model

--- test/unit/list_test.rb
+++ test/unit/list_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "A Name should be required" do
+  	list = List.new(:name => nil)
+  	assert_equal list.valid?, false
+  	assert_not_nil list.errors[:name]
+  end
+
+  test "Todos can be added to the List" do
+  	list = lists(:one)
+  	assert_difference('list.todos.size', +1){
+  		list.todos.create(:name => "Stuff to do")
+  	}
+  end
 end
