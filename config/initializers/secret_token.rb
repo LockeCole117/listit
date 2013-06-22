fc203c31772fb3e87a940d0c6e8118750e77c739
Thomas Cannon <tcannon@codemonkeydev.com>
2013-06-22 00:23:30 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listit::Application.config.secret_token = '29e430dc6ada42bdc4283ff49f151e1793a9735aa219bd98023b4f993571bfb35746e52b7683165b741716d49692d4083cb299db951de0dab8638420a23d1f21'
