d1fa0f59215e30bd2f39e683e0c9c7a34bb3ec93
Thomas Cannon <tcannon@codemonkeydev.com>
2013-06-22 20:52:17 -0400

    Basic App Styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def flash_all
+  	flash[:success] = "Some success message"
+  	flash[:error] 	= "Some failure message"
+  	flash[:notice] 	= "Some notice message"
+  end
 end
